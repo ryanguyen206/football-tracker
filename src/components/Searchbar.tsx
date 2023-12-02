beb3df87772f927f80a7ca7b98e11452ead0f680
ryanguyen206 <ryanninja595@gmail.com>
2023-12-02 12:57:03 -0800

    On team click, redirect to team page

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,16 +1,15 @@
 import React, {useState} from 'react'
 import {Input} from "@nextui-org/react";
-import { useQueryClient } from 'react-query';
 import { finalTeamStats } from '@/weeklyMatches';
 import useFetchAllTeamsQuery from '@/hooks/useFetchAllTeamsQuery';
+import Link from 'next/link'
 
 const Searchbar = () => {
-
     const {allTeams} = useFetchAllTeamsQuery()
     const [query, setQuery] = useState('')
     const [filteredItems, setFilteredItems] = useState<finalTeamStats[] | undefined>([])
 
-    const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+    const handleInputChange = (e : React.ChangeEvent<HTMLInputElement>) => {
         const userInput = e.target.value
         setQuery(userInput)
         if (userInput.length === 0) {
@@ -29,7 +28,7 @@ const Searchbar = () => {
                 type="email" 
                 placeholder="Search Team" 
                 value={query}
-                onChange= {(e) => handleChange(e)}
+                onChange= {(e) => handleInputChange(e)}
                 className="w-full"
             />
         }
@@ -37,9 +36,12 @@ const Searchbar = () => {
             filteredItems && filteredItems.length > 0 &&   
             <div className='absolute top-14 w-full border rounded-xl flex flex-col shadow-sm'>
                 {filteredItems?.map((team) => (
-                <div className='p-4 font-semibold hover:font-extrabold hover:cursor-pointer hover:bg-slate-200 ' key={team.Team}>
-                    <p className=''>{team.Team}</p>
-                </div>
+                    <Link href={`/team/${team.Team}`} onClick={() => setFilteredItems([])}>
+                        <div className='p-4 hover:cursor-pointer hover:bg-slate-200 ' key={team.Team} >
+                            <p className='font-semibold'>{team.Team}</p>
+                            <p className='font-medium'>{team.TeamName}</p>
+                        </div>
+                    </Link>
                 ))}
           </div>
         }
