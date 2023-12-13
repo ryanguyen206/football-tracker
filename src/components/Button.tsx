798f90e87e6d3b3d3054f0a59682b1649d461584
ryanguyen206 <ryanninja595@gmail.com>
2023-12-12 18:24:10 -0800

    Create a reusable offense or defense button

---
+++ src/components/Button.tsx
@@ -0,0 +1,42 @@
+import React, {useMemo, useState, FC} from 'react'
+import {Dropdown,DropdownTrigger,DropdownMenu,DropdownItem, Button} from "@nextui-org/react";
+import {Selection} from "@react-types/shared";
+import TableStats from './Table/TableStats';
+
+interface ButtonOffOrDefProps {
+    selected: Selection
+    setSelected: React.Dispatch<React.SetStateAction<Selection>>
+    selectedValue: string
+}
+
+const ButtonOffOrDef: FC<ButtonOffOrDefProps> = ({selected, setSelected, selectedValue}) => {
+
+
+  return (
+    <div id="teamStats" className='pb-20 text-center mt-28'>
+    <Dropdown className='mb-20'>
+      <DropdownTrigger>
+        <Button 
+          className="capitalize p-6 text-xl mb-20"
+        >
+          {selectedValue}
+        </Button>
+      </DropdownTrigger>
+      <DropdownMenu 
+        aria-label='Choose football week'
+        variant="flat"
+        disallowEmptySelection
+        selectionMode="single"
+        selectedKeys={selected}
+        onSelectionChange={setSelected}
+        className='text-black'
+      >
+        <DropdownItem key="Offense">Offense</DropdownItem>
+        <DropdownItem key="Defense">Defense</DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+    </div>
+  )
+}
+
+export default ButtonOffOrDef
--- src/components/Table/TeamStats.tsx
+++ src/components/Table/TeamStats.tsx
@@ -3,6 +3,7 @@ import React, {useEffect, useMemo, useState } from 'react'
 import {Dropdown,DropdownTrigger,DropdownMenu,DropdownItem,Button} from "@nextui-org/react";
 import {Selection} from "@react-types/shared";
 import TableStats from './TableStats';
+import ButtonOffOrDef from '../Button';
 
 
 const TeamStats = () => {
@@ -13,27 +14,7 @@ const TeamStats = () => {
   return (
     <div id="teamStats" className='pb-20 text-center mt-28'>
       <h2 className='text-5xl font-bold mb-10'>Stats</h2>
-    <Dropdown className='mb-20'>
-      <DropdownTrigger>
-        <Button 
-          className="capitalize p-6 text-xl mb-20"
-        >
-          {selectedValue}
-        </Button>
-      </DropdownTrigger>
-      <DropdownMenu 
-        aria-label='Choose football week'
-        variant="flat"
-        disallowEmptySelection
-        selectionMode="single"
-        selectedKeys={selected}
-        onSelectionChange={setSelected}
-        className='text-black'
-      >
-        <DropdownItem key="Offense">Offense</DropdownItem>
-        <DropdownItem key="Defense">Defense</DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
+      <ButtonOffOrDef selected={selected} setSelected={setSelected} selectedValue={selectedValue} />
         {selectedValue === 'Offense' ? 
             <TableStats 
                 headers={['Team', 'Touchdowns', 'Passing', 'Rushing']}
