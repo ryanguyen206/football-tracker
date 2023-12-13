5088ec6b16ce3a9d03663caf18ded8151f14a047
ryanguyen206 <ryanninja595@gmail.com>
2023-12-12 22:09:03 -0800

    Create new hook for allTeams

--- src/hooks/useFetchAllTeamsQuery.tsx
+++ src/hooks/useFetchAllTeamsQuery.tsx
@@ -1,43 +1,17 @@
-import { fetchAllTeamStats } from '@/helperFn/helper';
-import React, { useState } from 'react'
-import { useQuery } from 'react-query';
-import {offense, defense, finalTeamStats} from '@/interfaces'
+import { getAllTeams } from '@/helperFn/helper'
+import React from 'react'
+import { useQuery } from 'react-query'
 
 const useFetchAllTeamsQuery = () => {
-
-
-    const [allTeams, setAllTeams] = useState<finalTeamStats[]>([])
-
-
-    const {} = useQuery({
-        queryKey: ['allTeamStats'],
-        queryFn: () => fetchAllTeamStats(),
-        onSuccess: (data) => {
-          const transformedData : finalTeamStats[] = data.map((team: any) => {
-            const { Team, TeamName, Touchdowns, PassingYards, RushingYards, Takeaways, Sacks, OpponentPassingInterceptions } = team;
-            const offense: offense = {
-              Touchdowns,
-              PassingYards,
-              RushingYards,
-            };
-            const defense: defense = {
-              Takeaways,
-              Sacks,
-              OpponentPassingInterceptions,
-            };
-            return {
-              Team,
-              TeamName,
-              Offense: offense,
-              Defense: defense,
-            };
-          });
-          setAllTeams(transformedData);
+    const {data: allTeams} = useQuery({
+        queryKey:['allTeams'],
+        queryFn: () => getAllTeams(),
+        onSuccess: () => {
+            console.log(' me hieter')
         }
       })
-  
 
-  return {allTeams, setAllTeams}
+    return {allTeams}
 }
 
 export default useFetchAllTeamsQuery
